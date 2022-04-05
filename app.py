b31347e85669c69c2b83543cddd2457ab58ffe88
henriqferreira <henriquepfn@gmail.com>
2022-04-05 10:47:35 -0300

    Requirements e servidor flask no python

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from ast import If
+from flask import Flask, render_template
 
 app = Flask("__name__")
 
-app.route("/")
+@app.route("/")
+
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
