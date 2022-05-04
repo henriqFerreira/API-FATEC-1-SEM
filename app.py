8d3fb9b6fa1c40d79c9442f7ae2fb34a623d082f
Henrique Neto <henriqueneto1912@gmail.com>
2022-05-04 09:09:02 -0300

    :bug: fix: Corrigindo inicialização do banco de dados

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from flask import Flask, render_template
 import sqlite3
+from os.path import exists
 from db import db
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 class Conexao(db):
     def __init__(self, bancoDeDados):
-        self.bd = bancoDeDados
+        self.bancoDeDados = bancoDeDados
 
     def conectarBD(self):
-        db = db()
-        db.create_db()
-        conn = sqlite3.connect(self.bd+".sqlite")
+        conn = sqlite3.connect(self.bancoDeDados+".sqlite")
+        conn.row_factory = sqlite3.Row
         return conn
+    
+    def verificarDB(self):
+        cx = Conexao("data")
+        datab = db()
 
+        if exists(self.bancoDeDados+".sqlite"):
+            conn = cx.conectarBD()
+            cur = conn.cursor()
+            stmt = cur.execute("SELECT COUNT(*) FROM vagas").fetchone()[0]
+
+            if stmt <= 0:
+                datab.insert_data("./web-crawler/vagas-ti/vagas-ti.json")
+        else:
+            datab.create_db()
+            datab.insert_data("./web-crawler/vagas-ti/vagas-ti.json")
+
+@app.before_first_request
+def inicializar():
+    cx = Conexao("data")
+    cx.verificarDB()
+    return "Inicializando..."
+    
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -24,7 +45,14 @@ def cursos():
 
 @app.route("/vagas")
 def vagas():
-    return render_template("vagas.html")
+    cx = Conexao("data")
+    conn = cx.conectarBD()
+    cur = conn.cursor()
+
+    stmt = cur.execute("SELECT * FROM vagas").fetchall()
+    conn.commit()
+
+    return render_template("vagas.html", data=stmt)
 
 @app.route("/contato")
 def contato():
